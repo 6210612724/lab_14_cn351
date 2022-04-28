b9fc967451047b1422228f07219ddce506f8bc0e
6210612724 <6210612724@student.tu.ac.th>
2022-04-28 21:29:20 +0700

    step 6

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3003;
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json())
 app.use(express.static('public'));
+app.post('/login', function (req, res) {
+    console.log('Got login request with body', req.body);
+    if (req.body && req.body.username && req.body.password) {
+        res.send({ message: 'Hello!' });
+    } else {
+        res.sendStatus(400);
+    }
+});
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
--- public/client.js
+++ public/client.js
@@ -1,1 +1,20 @@
-alert('Hello, World!');
+window.onload = function () {
+    document.getElementById('login-button').addEventListener('click', function () {
+        const username = document.getElementById('username').value
+        const password = document.getElementById('password').value
+        const body = JSON.stringify({ username: username, password: password });
+        const httpReq = new XMLHttpRequest();
+        httpReq.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                document.getElementById('login-error').innerHTML = '';
+                document.getElementById('login-result').innerHTML = `Login request succeeded with response ${this.responseText}`;
+            } else if (this.readyState === 4) {
+                document.getElementById('login-error').innerHTML = `Server responded with error code ${this.status}`;
+                document.getElementById('login-result').innerHTML = '';
+            }
+        };
+        httpReq.open('POST', '/login', true);
+        httpReq.setRequestHeader('Content-Type', 'application/json');
+        httpReq.send(body);
+    });
+}
