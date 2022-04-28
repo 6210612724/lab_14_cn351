4d0156d5db663e06c61722001e1ea194864158fd
6210612724 <6210612724@student.tu.ac.th>
2022-04-28 21:47:17 +0700

    step 8

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3003;
 const bodyParser = require('body-parser');
 const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
 const users = [{ username: 'foouser', password: '111', passwordHash: '' }];
 users.forEach(function (userObj) {
@@ -12,6 +14,30 @@ users.forEach(function (userObj) {
         console.log('User', userObj.username, 'has password hash', userObj.passwordHash);
     });
 });
+
+
+let jwtPrivateKey = null;
+let jwtPublicKey = null;
+try {
+    jwtPrivateKey = fs.readFileSync('jwt_priv.pem');
+    jwtPublicKey = fs.readFileSync('jwt_pub.pem');
+} catch (e) {
+    // Need to generate public/private keypair for JWT
+    console.log('Need to generate key! Run the following commands:');
+    console.log('openssl genrsa -des3 -out jwt.pem -passout pass:foobar 2048');
+    console.log('openssl rsa -in jwt.pem -outform PEM -pubout -out jwt_pub.pem -passin pass:foobar');
+    console.log('openssl rsa -in jwt.pem -outform PEM -out jwt_priv.pem -passin pass:foobar');
+    console.log('rm -f jwt.pem');
+    throw new Error('Generate RSA keys first');
+}
+
+
+function generateJwt(username) {
+    return jwt.sign({
+        username: username,
+        exp: new Date().valueOf() + (1000 * 60 * 60 * 6) // 6 hours
+    }, jwtPrivateKey, { algorithm: 'RS256' });
+}
 
 app.use(bodyParser.json())
 app.use(express.static('public'));
@@ -27,7 +53,10 @@ app.post('/login', function (req, res) {
             argon2.verify(userObj.passwordHash, req.body.password).then((success) => {
                 if (success) {
                     console.log('Successful hash verification');
-                    res.send({ message: 'Success!' });
+                    res.send({
+                        username: userObj.username,
+                        token: generateJwt(userObj.username)
+                    });
                 } else {
                     console.log('Bad password for user', userObj.username);
                     res.sendStatus(400);
