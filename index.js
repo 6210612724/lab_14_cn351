9b59d20fe1b703b31d0988210db32d95391722c2
6210612724 <6210612724@student.tu.ac.th>
2022-04-28 21:26:49 +0700

    step 4

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3003;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
