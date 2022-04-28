25d592925d12d8aeb53aac7b9ca6ab761fee2fb5
6210612724 <6210612724@student.tu.ac.th>
2022-04-28 21:31:22 +0700

    step 7

--- index.js
+++ index.js
@@ -2,14 +2,43 @@ const express = require('express');
 const app = express();
 const port = 3003;
 const bodyParser = require('body-parser');
+const argon2 = require('argon2');
+
+const users = [{ username: 'foouser', password: '111', passwordHash: '' }];
+users.forEach(function (userObj) {
+    argon2.hash(userObj.password).then(function (hash) {
+        userObj.passwordHash = hash;
+        delete userObj.password;
+        console.log('User', userObj.username, 'has password hash', userObj.passwordHash);
+    });
+});
 
 app.use(bodyParser.json())
 app.use(express.static('public'));
 app.post('/login', function (req, res) {
     console.log('Got login request with body', req.body);
     if (req.body && req.body.username && req.body.password) {
-        res.send({ message: 'Hello!' });
+        const matchingUsers = users.filter(obj => { return obj.username === req.body.username });
+        if (matchingUsers.length == 0) {
+            console.log('Got POST /login for username not found in user database');
+            res.sendStatus(400);
+        } else {
+            const userObj = matchingUsers[0];
+            argon2.verify(userObj.passwordHash, req.body.password).then((success) => {
+                if (success) {
+                    console.log('Successful hash verification');
+                    res.send({ message: 'Success!' });
+                } else {
+                    console.log('Bad password for user', userObj.username);
+                    res.sendStatus(400);
+                }
+            }).catch((error) => {
+                console.log('Unexpected error validating hash:', error);
+                res.sendStatus(500);
+            });
+        }
     } else {
+        console.log('Got POST /login without username and password in body');
         res.sendStatus(400);
     }
 });
